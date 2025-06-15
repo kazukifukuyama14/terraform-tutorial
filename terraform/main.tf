9209a23deb35b881779dbb0a9f852a69bdeecdde
kazukifukuyama <p.t.c_vdl_blue@icloud.com>
2025-06-15 16:29:47 +0900

    Add EC2 document and Add Terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,4 +6,14 @@ provider "aws" {
 resource "aws_instance" "hello-world" {
   ami           = "ami-0bb2c57f7cfafb1cb"
   instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+  user_data = <<EOF
+#!/bin/bash
+amazon-linux-extras install -y nginx1.12
+systemctl start nginx
+EOF
+
+  user_data_replace_on_change = true
 }
