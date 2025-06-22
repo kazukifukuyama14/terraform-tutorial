a9580352c0afd2d65e131f697d9ec0682a839b5b
kazukifukuyama <p.t.c_vdl_blue@icloud.com>
2025-06-23 08:13:20 +0900

    Add random provider setting

--- terraform-aws-infrastructure/main.tf
+++ terraform-aws-infrastructure/main.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = { # randomプロバイダー追加
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
   }
 }
 
